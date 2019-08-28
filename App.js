aafa3aa67947f01f786c5882bd33f688fc1a5683
guimao dai <wyyxdgm@163.com>
2019-08-28 16:43:01 +0800

    flex

--- App.js
+++ App.js
@@ -4,17 +4,27 @@ import { StyleSheet, Text, View } from 'react-native';
 export default class LotsOfStyles extends Component {
   render() {
     return (
-      <View>
+      <View style={{heihgt: '100%', display: 'flex', alignItems: 'center', justifyContent: 'center'}} >
+      <View style={{flex: 1, flexDirection: 'column'}}>
+        <View style={{width: 200, height: 200, backgroundColor: 'powderblue',...styles.center}} >
         <Text style={styles.red}>just red</Text>
+        </View>
+        <View style={{width: 200, height: 200, backgroundColor: 'skyblue',...styles.center}} >
         <Text style={styles.bigBlue}>just bigBlue</Text>
+        </View>
+        <View style={{width: 200, height: 200, backgroundColor: 'steelblue',...styles.center}} >
         <Text style={[styles.bigBlue, styles.red]}>bigBlue, then red</Text>
-        <Text style={[styles.red, styles.bigBlue]}>red, then bigBlue</Text>
+        </View>
+      </View>
       </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
+  center:{
+    display: 'flex', alignItems: 'center', justifyContent: 'center'
+  },
   bigBlue: {
     color: 'blue',
     fontWeight: 'bold',
