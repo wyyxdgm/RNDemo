33969b9b88d252835df669a0d5b787d6684a650f
guimao dai <wyyxdgm@163.com>
2019-08-28 16:03:14 +0800

    banner

--- App.js
+++ App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import { Text, View } from 'react-native';
+import { Image } from 'react-native';
 
-export default class HelloWorldApp extends Component {
+export default class Bananas extends Component {
   render() {
+    let pic = {
+      uri: 'https://upload.wikimedia.org/wikipedia/commons/d/de/Bananavarieties.jpg'
+    };
     return (
-        <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-          <Text>Hello, world!</Text>
-        </View>
+      <Image source={pic} style={{width: '100%', height: 210}} />
     );
   }
 }
