94fd4a0ba0f442a308df12dd31392ed04b7003ec
guimao dai <wyyxdgm@163.com>
2019-08-28 16:05:34 +0800

    component Greeting

--- App.js
+++ App.js
@@ -1,14 +1,24 @@
 import React, { Component } from 'react';
 import { Text, View } from 'react-native';
-import { Image } from 'react-native';
 
-export default class Bananas extends Component {
+class Greeting extends Component {
   render() {
-    let pic = {
-      uri: 'https://upload.wikimedia.org/wikipedia/commons/d/de/Bananavarieties.jpg'
-    };
     return (
-      <Image source={pic} style={{width: '100%', height: 210}} />
+      <View style={{alignItems: 'center', marginTop: 50}}>
+        <Text>Hello {this.props.name}!</Text>
+      </View>
+    );
+  }
+}
+
+export default class LotsOfGreetings extends Component {
+  render() {
+    return (
+      <View style={{alignItems: 'center'}}>
+        <Greeting name='Rexxar' />
+        <Greeting name='Jaina' />
+        <Greeting name='Valeera' />
+      </View>
     );
   }
 }
